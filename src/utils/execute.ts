8f9f804341ff2c905417bd2abbbad459aea0a37f
Joakim Hedlund <contact@joakimhedlund.com>
2023-03-02 08:48:04 +0100

    fix: include failing command in error messages when exec fails

--- src/utils/execute.ts
+++ src/utils/execute.ts
@@ -10,7 +10,11 @@ export default async (command: string): Promise<string> => await new Promise((re
   exec(`cd "${process.cwd()}" && ${command}`, { shell: 'bash', maxBuffer }, (err, stdout, stderr) => {
     if (err) {
       // when `command` itself fails its error will be printed to stderr
-      if (stderr) return reject(new Error(stderr))
+      if (stderr) {
+        // include the failing command in the error message to make debugging easier
+        const errmsg = [command, stderr].join('\n')
+        return reject(new Error(errmsg))
+      }
       // when child_process.exec fails the source error will be in `err`
       return reject(err)
     }
